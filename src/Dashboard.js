41e5a2aaa058d79451cb7e2b075dd7113b22e2e1
Javohir Nurmatjonov <javoxirone@gmail.com>
2021-11-02 22:08:26 +0500

    asd

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,12 +1,7 @@
 import React, {useState} from 'react'
 import './Styles/style.scss'
-
-import Logo from './Img/logo.svg'
 // Components
-import Sidebar from "./Components/Sidebar/Sidebar"
 import Main from "./Components/Main/Main"
-import Header from "./Components/Header/Header"
-import Footer from "./Components/Footer/Footer"
 
 
 function Dashboard(props) {
